199b8504ecace553dd01d4932bc9270049bd0d75
Vincent Wang <vincent@linsong.org>
2017-12-04 11:40:30 +0800

    reorganize and put all files under alarmdb folder
    with the help of updating include_path config in php, we can put all
    alarmdb related files under one folder. in CPT Graphics, the place for
    plugin will be 'cpt/plugins', CPT Graphics will detect this folder and
    will render a plugin button on UI when it finds any plugin available.

--- Ftp/sdcard/cpt/plugins/alarmdb/alarmdb.php
+++ Ftp/sdcard/cpt/plugins/alarmdb/alarmdb.php
@@ -11,22 +11,20 @@
 
 //ini_set('display_errors',1); error_reporting(E_ALL);
 
+set_include_path(get_include_path() . PATH_SEPARATOR . '../../app');
+
 include_once "db.php";
 include_once "settings.php";
 include_once "feature_control.php";
 include_once "base_controller.php";
 
-class GraphicController extends BaseController {
+class AlarmDBController extends BaseController {
 	protected function signinRequired() {
 		return true;
 	}
-
-	public function cptToolsBackupUrl() {
-		return $this->makeUrl('../CPT-Tools.zip');
-	}
 }
 
-$controller = new GraphicController();
+$controller = new AlarmDBController();
 $controller->run();
 $u = $controller->curUser();
 
@@ -52,11 +50,11 @@ $u = $controller->curUser();
 			$(document).ready(function () {
 				//Set-up AlarmDB before render it to web-page
 				AlarmDbSettings.href = "https://github.com/automatikas/AlarmDB-EasyIO/releases/tag/"; //Link to AlarmDB-EasyIO github branch.
-				AlarmDbSettings.api.location = "../app/alarmdb_exporter.php"; 		// Link to DB connector
+				AlarmDbSettings.api.location = "alarmdb_exporter.php"; 		// Link to DB connector
 				AlarmDbSettings.api.headers.auth_key = ""; 							// EasyIO FG/FS auth_key to access alarmdb_exporter.php without login. Not needed within same host
 				AlarmDbSettings.ui.loadContainer.divId = "#myAlarmContainer";		// HTLM DIV ID where to render AlarmDb
 				AlarmDbSettings.ui.user.name = "<?php echo $u->attr('name') ?>";	// Active user name from FG/FS used to acknowledge alarms and for writing alarm notes
-				AlarmDbSettings.ui.logo.img = "../alarmdb/img/logo.png";			// Link to logo image of AlarmDB. For custom logos use 300x100px images
+				AlarmDbSettings.ui.logo.img = "img/logo.png";			// Link to logo image of AlarmDB. For custom logos use 300x100px images
 				AlarmDbSettings.ui.controlBar.usermenu.enabled = false;				// Native AlarmDB API is not used so to avoid login conflicts disable AlarmDB user account management
 				AlarmDbSettings.ui.controlBar.export.formats[1].enabled = false; 	// Disable XML export. Missing SimpleXMLElement converter. Workaround for XML within next release
 				//Initialize and render AlarmDb to the page
