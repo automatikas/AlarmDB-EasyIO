93ef4529280bc616ae1aef59babe640877351384
AJ <automatikas@gmail.com>
2018-11-03 21:24:17 +0100

    v2.08 prep

--- alarmdb/index.php
+++ alarmdb/index.php
@@ -51,20 +51,8 @@ $u = $controller->curUser();
 		
 		<script type="text/javascript">
 			$(document).ready(function () {
-				//Set-up AlarmDB before render it to web-page
-				AlarmDbSettings.href = "https://github.com/automatikas/AlarmDB-EasyIO/releases/tag/"; //Link to AlarmDB-EasyIO github branch.
-				AlarmDbSettings.api.location = "alarmdb_exporter.php"; 				// Link to DB connector
-				AlarmDbSettings.api.headers.auth_key = ""; 							// EasyIO FG/FS auth_key to access alarmdb_exporter.php without login. Not needed within same host
-				AlarmDbSettings.ui.loadContainer.divId = "#myAlarmContainer";		// HTLM DIV ID where to render AlarmDb
-				AlarmDbSettings.ui.user.name = "<?php echo $u->attr('name') ?>";	// Active user name from FG/FS used to acknowledge alarms and for writing alarm notes
-				AlarmDbSettings.ui.logo.img = "img/logo.png";						// Link to logo image of AlarmDB. For custom logos use 300x100px images
-				AlarmDbSettings.ui.controlBar.usermenu.enabled = false;				// Native AlarmDB API is not used so to avoid login conflicts disable AlarmDB user account management
-				AlarmDbSettings.ui.controlBar.export.formats[1].enabled = false; 	// Disable XML export. Missing SimpleXMLElement converter. Workaround for XML within next release
-				AlarmDbSettings.ui.dateselector.enabled = true;						// Date range selector for alarms
-				AlarmDbSettings.ui.tabs.display = true;								// Hide tabs only show active alarm tabs.(more like log view style)
-				AlarmDbSettings.ui.table.action.buttons.ackn.enabled = true;		// disable alarm acknowledge button.
-				AlarmDbSettings.ui.table.action.buttons.delete.enabled = true;		// enable alarm delete button.				
-				//Initialize and render AlarmDb to the page
+				AlarmDbSettings.ui.loadContainer.divId = "#myAlarmContainer";
+				AlarmDbSettings.ui.user.name = "<?php echo $u->attr('name') ?>";
 				loadAlarmDB();
 			});
 		</script>	
